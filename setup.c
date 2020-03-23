10a782f6b8d7e0606629e0f8cb1ff1af409539af
Lawrenc3X <57817583+Lawrenc3X@users.noreply.github.com>
2020-03-23 16:50:25 -0700

    Summary (required)

--- setup.c
+++ setup.c
@@ -10,6 +10,63 @@
 
 //  #define DATA_SIZE 1000
 
+char * ext(char fname[])
+{
+    int index = 0;
+    while (true)
+    {
+        if (fname[index] == '.')
+        {
+            return fname + index;
+        }
+        index += 1;
+    }
+}
+
+char * nm(char fname[])
+{
+    int index = 0;
+    static char string[100];
+
+    while (true)
+    {
+        if (fname[index] == '.')
+        {
+            break;
+        }
+        string[index] = fname[index];
+
+        index += 1;
+    }
+
+    return string;
+}
+
+bool equal(char s1[], char s2[])
+{
+    int index = 0;
+
+    while (true)
+    {
+        if (s1[index] == '\0' && s2[index] == '\0')
+        {
+            return true;
+        }
+
+        if (s1[index] != s2[index])
+        {
+            return false;
+        }
+
+        if (s1[index] == '\0' ^ s2[index] == '\0')
+        {
+            return false;
+        }
+
+        index += 1;
+    }
+}
+
 bool exists(const char *fname)
 {
     if(access(fname, F_OK) != -1)
@@ -23,6 +80,12 @@ bool exists(const char *fname)
 
 int main(int argc, char * argv[])
 {
+    char * filename = argv[1];
+    char * name = nm(filename);
+    char * extension = ext(filename);
+
+    printf("filename: %s name: %s extension: %s\n", filename, name, extension);
+
     if (!exists("makefile") )
     {
         FILE * makefile;
@@ -34,7 +97,24 @@ int main(int argc, char * argv[])
             exit(EXIT_FAILURE);
         }
 
-        // code
+        char * compiler = equal(".c", extension)? "CC" : "CXX";
+        char * flags = equal(".c", extension) ? "CFLAGS" : "CXXFLAGS";
+
+        fprintf(makefile, "CC = gcc\
+CXX = g++\
+CFLAGS = -g\
+CXXFLAGS = -g\
+\
+setup: setup.c\
+ $(%s) $(%s) -o %s %s\
+\
+all: setup.c\
+ $(%s) $(%s) -o %s %s\
+\
+clean:\
+     rm -r %s %s%s", compiler, flags, name, filename, compiler, flags, name, filename, name, name, ".dSYM");
+
+        fclose(makefile);
     } else
     {
         printf("makefile already exists\n");
@@ -43,7 +123,6 @@ int main(int argc, char * argv[])
     if (!exists(".gitignore") )
     {
         FILE * gitignore;
-        printf("yes\n");
 
         gitignore = fopen(".gitignore", "a");
         if (gitignore == NULL)
@@ -52,7 +131,115 @@ int main(int argc, char * argv[])
             exit(EXIT_FAILURE);
         }
 
-        // code
+        if (equal(".c", extension))
+        {
+            fprintf(gitignore, "# Prerequisites\
+*.d\
+\
+# Object files\
+*.o\
+*.ko\
+*.obj\
+*.elf\
+\
+# Linker output\
+*.ilk\
+*.map\
+*.exp\
+\
+# Precompiled Headers\
+*.gch\
+*.pch\
+\
+# Libraries\
+*.lib\
+*.a\
+*.la\
+*.lo\
+\
+# Shared objects (inc. Windows DLLs)\
+*.dll\
+*.so\
+*.so.*\
+*.dylib\
+\
+# Executables\
+*.exe\
+*.out\
+*.app\
+*.i*86\
+*.x86_64\
+*.hex\
+\
+# Debug files\
+*.dSYM/\
+*.su\
+*.idb\
+*.pdb\
+\
+# Kernel Module Compile Results\
+*.mod*\
+*.cmd\
+.tmp_versions/\
+modules.order\
+Module.symvers\
+Mkfile.old\
+dkms.conf\
+\
+# .dSYM\
+*.dSYM\
+\
+# .DS_Store\
+.DS_Store\
+\
+# Swap files\
+*.swp");
+        } else
+        {
+            fprintf(gitignore, "# Prerequisites\
+*.d\
+\
+# Compiled Object files\
+*.slo\
+*.lo\
+*.o\
+*.obj\
+\
+# Precompiled Headers\
+*.gch\
+*.pch\
+\
+# Compiled Dynamic libraries\
+*.so\
+*.dylib\
+*.dll\
+\
+# Fortran module files\
+*.mod\
+*.smod\
+\
+# Compiled Static libraries\
+*.lai\
+*.la\
+*.a\
+*.lib\
+\
+# Executables\
+*.exe\
+*.out\
+*.app\
+\
+# .dSYM\
+*.dSYM\
+\
+# .DS_Store\
+.DS_Store\
+\
+# Swap files\
+*.swp");
+        }
+
+        fclose(gitignore);
     } else
     {
         printf("gitignore already exists\n");
